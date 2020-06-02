d402675313d3fe17fc4969c2ccb3b065cf7b79ea
penumbra23 <glbranimir@gmail.com>
2020-06-02 09:38:35 +0200

    Add tests for BasicFilter and Murmur hash

---
+++ Bloomy.Test/FilterTest.cs
@@ -0,0 +1,69 @@
+﻿using Bloomy.Lib.Filter;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Bloomy.Test
+{
+    public class FilterTest
+    {
+        public readonly string[] WordList = new string[] 
+        { 
+            "pasta", "subaru", "activist", "painting", "resident", "broken", "channel", "despite", "collision", "Peter",
+            "cake", "take", "make", "bake", "rake", "Fake", "aake", "dake", "eake", "wake",
+            "otorhinolaryngological", "radioimmunoelectrophoresis", "hepaticocholangiogastrostomy", "immunoelectrophoretically",
+            "programmatically", "Thyroparathyroidectomized", "incomprehensibilities", "dichlorodifluoromethane",
+            "password1234!", "BobAndMary$$56", "admin!_??", "asdjhbqiw74ey92498qyfawh82y60t298q", "1!!$%%auhsdu%$#$#@Uhffuh%^$",
+            "$%(*&^%(*&Yiouhgasdo8089&%)*&U%)(dfagbdb*)O^&U)(*&%)(YH(O)UY%)#*(oh", "aoIHDO*Y*%^&)*HDALSJGH)(*%&)(DDSD"
+        };
+
+        [Fact]
+        public void Murmur3FilterCheck()
+        {
+            BasicFilter filter = new BasicFilter(50, HashFunc.Murmur3);
+            foreach(string str in WordList)
+                filter.Insert(str);
+
+            foreach(string str in WordList)
+                Assert.True(filter.Check(str).Present);
+
+            // This was brute-forced not to be in the set
+            Assert.False(filter.Check("notInThere4").Present);
+        }
+
+        [Fact]
+        public void SHA256FilterCheck()
+        {
+            BasicFilter filter = new BasicFilter(50, HashFunc.SHA256);
+            foreach (string str in WordList)
+                filter.Insert(str);
+
+            foreach (string str in WordList)
+                Assert.True(filter.Check(str).Present);
+
+            // This was brute-forced not to be in the set
+            Assert.False(filter.Check("notInThere4").Present);
+
+            // This was brute-forced to have matching bits although it's not in the set
+            Assert.True(filter.Check("notInThere11").Present);
+        }
+
+        [Fact]
+        public void SHA512FilterCheck()
+        {
+            BasicFilter filter = new BasicFilter(50, HashFunc.SHA512);
+            foreach (string str in WordList)
+                filter.Insert(str);
+
+            foreach (string str in WordList)
+                Assert.True(filter.Check(str).Present);
+
+            // This was brute-forced not to be in the set
+            Assert.False(filter.Check("notInThere4").Present);
+
+            // This was brute-forced to have matching bits although it's not in the set
+            Assert.True(filter.Check("notInThere11").Present);
+        }
+    }
+}
---
+++ Bloomy.Test/HashTest.cs
@@ -0,0 +1,26 @@
+﻿using Bloomy.Lib.HashFunction;
+using System.Text;
+using Xunit;
+
+namespace Bloomy.Test
+{
+    public class HashTest
+    {
+        [Fact]
+        public void Murmur3Characteristics()
+        {
+            var hashF = Murmur3.Create();
+            var hash = hashF.ComputeHash(Encoding.ASCII.GetBytes("9089089whatevathisstringshouldbe123!!!"));
+            Assert.Equal(8, hash.Length);
+        }
+
+        [Fact]
+        public void Murmur3SimilarStrings()
+        {
+            var hash = Murmur3.Create();
+            var hash1 = hash.ComputeHash(Encoding.ASCII.GetBytes("awesome1"));
+            var hash2 = hash.ComputeHash(Encoding.ASCII.GetBytes("awesome1."));
+            Assert.NotEqual(hash1, hash2);
+        }
+    }
+}
